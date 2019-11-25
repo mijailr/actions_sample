bbbda442a0a28e4bb3728314c05e5d74b5ba6b3d
Mijail Rondon <mijail@riseup.net>
2019-11-25 03:58:12 -0500

    Add specs definitions

--- lib/actions_sample.ex
+++ lib/actions_sample.ex
@@ -1,12 +1,19 @@
 defmodule ActionsSample do
+  @moduledoc """
+  This module operate some math
+  """
+
+  @spec add(number, number) :: number
   def add(x, y) do
     x + y
   end
 
+  @spec multiply(number, number) :: number
   def multiply(x, y) do
     x * y
   end
 
+  @spec subtract(number, number) :: number
   def subtract(x, y) do
     x - y
   end
